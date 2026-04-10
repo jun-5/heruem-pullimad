2f1772921f211c969ea23d60cb9fdc8bba9d411a
jun-5 <rkd7997@gmail.com>
2026-04-10 16:24:31 +0900

    initial Commit

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "풀림 스타일 랜딩",
+  description:
+    "검색 최적화와 퍼포먼스 마케팅을 소개하는 단일 페이지형 Next.js 랜딩 예시",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
